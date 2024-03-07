5537bef9c6ed6081f9826089940b8f08d80e3d80
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2024-03-07 19:44:45 -0300

    database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ProjectsModule } from './modules/projects/projects.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UsersModule,
+    ProjectsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
